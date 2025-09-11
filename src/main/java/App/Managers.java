588601e14237f2e5c238a89d1741d09e67774258
ananda <gomboevananda@gmail.com>
2025-09-11 16:57:35 +0800

    test

--- src/main/java/App/Managers.java
+++ src/main/java/App/Managers.java
@@ -6,7 +6,7 @@ import App.Tasks.*;
 public class Managers {
 
     public static TaskManager getDefault(){
-        return new FileBackedTaskManager("C:\\temp\\taskTest.txt");
+        return new FileBackedTaskManager("D:\\dev\\taskTest.txt");
     }
     public static HistoryManager getDefaultHistory(){
         return new InMemoryHistoryManager();
